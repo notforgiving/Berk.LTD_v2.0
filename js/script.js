ce5f07a57ac8648ee93069fece44bcc1735f4740
notforgiving <notforgiving@list.ru>
2025-03-19 20:48:52 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const pages = document.querySelector(".pages");
 const pointsOfPages = pages.querySelectorAll("span");
+const videoBg = document.querySelector("video");
 
 document.querySelector(
   "#size"
@@ -13,31 +14,33 @@ setTimeout(() => {
 }, SPEEDANIMATIONS);
 
 window.addEventListener("load", (event) => {
-  $("#fullpage").fullpage({
-    css3: false,
-    responsiveWidth: 1250,
-    afterLoad: function (anchorLink, index) {
-      pointsOfPages[index - 1].classList.add("active");
-    },
-    onLeave: function (index, nextIndex, direction) {
-      pointsOfPages[index - 1].classList.remove("active");
-      pointsOfPages[nextIndex - 1].classList.add("active");
-    },
-  });
-  $.fn.fullpage.setAllowScrolling(false);
+  if (videoBg) {
+    $("#fullpage").fullpage({
+      css3: false,
+      responsiveWidth: 1250,
+      afterLoad: function (anchorLink, index) {
+        pointsOfPages[index - 1].classList.add("active");
+      },
+      onLeave: function (index, nextIndex, direction) {
+        pointsOfPages[index - 1].classList.remove("active");
+        pointsOfPages[nextIndex - 1].classList.add("active");
+      },
+    });
+    $.fn.fullpage.setAllowScrolling(false);
 
-  setTimeout(() => {
-    body.classList.remove("welcome");
-    body.classList.add("bg");
-    document.querySelector("video").play();
-  }, SPEEDANIMATIONS * 2);
+    setTimeout(() => {
+      body.classList.remove("welcome");
+      body.classList.add("bg");
+      document.querySelector("video").play();
+    }, SPEEDANIMATIONS * 2);
 
-  setTimeout(() => {
-    body.classList.add("overlay");
-    body.classList.add("title");
-  }, SPEEDANIMATIONS * 3);
+    setTimeout(() => {
+      body.classList.add("overlay");
+      body.classList.add("title");
+    }, SPEEDANIMATIONS * 3);
 
-  setTimeout(() => {
-    $.fn.fullpage.setAllowScrolling(true);
-  }, SPEEDANIMATIONS * 5.5);
+    setTimeout(() => {
+      $.fn.fullpage.setAllowScrolling(true);
+    }, SPEEDANIMATIONS * 5.5);
+  }
 });
