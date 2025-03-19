f0b9aa20840d7b099cbab01ee89f01feab86e555
notforgiving <notforgiving@list.ru>
2025-03-19 19:50:54 +0300

    ++

---
+++ js/customCursor.js
@@ -0,0 +1,59 @@
+const cursor = document.querySelector("#cursor");
+const cursorFollowers = cursor.querySelectorAll("span");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let tail = [];
+
+let mouseMove = false;
+let returnInStart = false;
+
+setInterval(() => {
+  mouseMove = false;
+}, 30);
+
+const moveEvent = (event) => {
+  cursor.style.opacity = '1';
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+
+  tail.push({
+    mouseX: mouseX - cursor.clientWidth / 2,
+    mouseY: mouseY - cursor.clientHeight / 2,
+  });
+
+  if (tail.length > 500) {
+    tail.shift();
+  }
+
+  mouseMove = true;
+}
+
+document.addEventListener("mousemove", moveEvent);
+document.addEventListener("touchmove", moveEvent);
+
+TweenMax.to({}, 0.01, {
+  repeat: -1,
+  onRepeat: function () {
+    cursorFollowers.forEach((item, index) => {
+      if (!!tail.length && tail.length - (8 * index + 1) >= 0) {
+        returnInStart = false;
+        TweenMax.set(item, {
+          css: {
+            left: tail[tail.length - (8 * index + 1)].mouseX,
+            top: tail[tail.length - (8 * index + 1)].mouseY,
+            opacity: mouseMove ? "1" : "0",
+          },
+        });
+      }
+    });
+
+    TweenMax.set(cursor, {
+      css: {
+        left: mouseX - cursor.clientWidth / 2,
+        top: mouseY - cursor.clientHeight / 2,
+      },
+    });
+  },
+});
---
+++ js/script.js
@@ -0,0 +1,44 @@
+const pages = document.querySelector(".pages");
+const pointsOfPages = pages.querySelectorAll("span");
+
+const startvideos = () => {
+  const videos = document.querySelectorAll("video");
+  videos.forEach((item) => {
+    item.play();
+  });
+};
+
+$("#fullpage").fullpage({
+  css3: false,
+  responsiveWidth: 1250,
+  afterLoad: function (anchorLink, index) {
+    pointsOfPages[index - 1].classList.add("active");
+  },
+  onLeave: function (index, nextIndex, direction) {
+    pointsOfPages[index - 1].classList.remove("active");
+    pointsOfPages[nextIndex - 1].classList.add("active");
+  },
+});
+
+// welcome
+const body = document.querySelector("body");
+$.fn.fullpage.setAllowScrolling(false);
+const SPEEDANIMATIONS = 1;
+setTimeout(() => {
+  body.classList.add("welcome");
+}, SPEEDANIMATIONS);
+
+setTimeout(() => {
+  body.classList.remove("welcome");
+  body.classList.add("bg");
+  startvideos();
+}, SPEEDANIMATIONS * 2);
+
+setTimeout(() => {
+  body.classList.add("overlay");
+  body.classList.add("title");
+}, SPEEDANIMATIONS * 3);
+
+setTimeout(() => {
+  $.fn.fullpage.setAllowScrolling(true);
+}, SPEEDANIMATIONS * 5.5);
