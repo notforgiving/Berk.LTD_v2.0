36a4bf1abec866d37982540242ad0c28a2241dfd
notforgiving <notforgiving@list.ru>
2025-03-19 20:32:00 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const startvideos = () => {
 };
 
 const body = document.querySelector("body");
-const SPEEDANIMATIONS = 1500;
+const SPEEDANIMATIONS = 1000;
 
 setTimeout(() => {
   body.classList.add("welcome");
@@ -28,20 +28,19 @@ window.onload = () => {
     },
   });
   $.fn.fullpage.setAllowScrolling(false);
+  
   setTimeout(() => {
     body.classList.remove("welcome");
     body.classList.add("bg");
     startvideos();
   }, SPEEDANIMATIONS * 2);
-  
+
   setTimeout(() => {
     body.classList.add("overlay");
     body.classList.add("title");
   }, SPEEDANIMATIONS * 3);
-  
+
   setTimeout(() => {
     $.fn.fullpage.setAllowScrolling(true);
   }, SPEEDANIMATIONS * 5.5);
-  
 };
-
